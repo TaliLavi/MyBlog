a010b442d48411259531d0169bd6c51414afb1ab
Talulali <tali@lavi.fm>
2014-12-27 02:22:06 +0000

    Dynamically create tags

--- my_blog.js
+++ my_blog.js
@@ -1,9 +1,22 @@
+//list of tags to be used to categorise posts
+TAGS = ["Cats","Bats","Wizards","Witches","Evil Pumpkins"];
+
+
+
+//create checkboxes dynamically based on TAGS
+function populateTagCheckboxes(){
+	var tagsHTML = "";
+	for (var i=0; i<TAGS.length; i++){
+		tagsHTML += "<li><input type=\"checkbox\" name=\"tag\" value=\""+TAGS[i]+"\" id=\"tag"+i+"\"> "+TAGS[i]+"</li>"
+	}
+	document.getElementById("inputTags").innerHTML = tagsHTML
+}
 
 
 //check which tags were selected
 function getCheckedTags(){
 	var checkedTags = [];
-	for (var i=0; i<5; i++){
+	for (var i=0; i<TAGS.length; i++){
 		var element = document.getElementById("tag" + i);
 		if (element.checked){
 			checkedTags[checkedTags.length] = element.value;
@@ -15,12 +28,12 @@ function getCheckedTags(){
 
 //create a string from the checked tags in order to later display it nicely
 function formatTags(){
-    var tagsText ="";
+    var tagsHTML ="";
     var checkedTags = getCheckedTags();
     for (var i=0; i<checkedTags.length; i++){
-    	tagsText += "<li>"+checkedTags[i]+"</li>";
+    	tagsHTML += "<li>"+checkedTags[i]+"</li>";
     }
-    return tagsText
+    return tagsHTML
 }
 
 //submit and display new post
