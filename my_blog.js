7637ea400ae6d555c6097834c76528fd91193ed1
Talulali <tali@lavi.fm>
2014-12-27 01:47:57 +0000

    Added tags

--- my_blog.js
+++ my_blog.js
@@ -1,6 +1,34 @@
 
-function submitNewPost() {
-    var Title = document.getElementById("inputTitle").value;
-    var Content = document.getElementById("inputContent").value;
-    document.getElementById("latestPost").innerHTML = Title + "<br>" + Content;
+
+//check which tags were selected
+function getCheckedTags(){
+	var checkedTags = [];
+	for (var i=0; i<5; i++){
+		var element = document.getElementById("tag" + i);
+		if (element.checked){
+			checkedTags[checkedTags.length] = element.value;
+		}
+	}
+	return checkedTags;
 }
+
+
+//create a string from the checked tags in order to later display it nicely
+function formatTags(){
+    var tagsText ="";
+    var checkedTags = getCheckedTags();
+    for (var i=0; i<checkedTags.length; i++){
+    	tagsText += "<li>"+checkedTags[i]+"</li>";
+    }
+    return tagsText
+}
+
+//submit and display new post
+function submitNewPost() {
+    document.getElementById("title").innerHTML = document.getElementById("inputTitle").value;
+    document.getElementById("content").innerHTML = document.getElementById("inputContent").value;
+    document.getElementById("tags").innerHTML = formatTags();
+}
+
+
+
